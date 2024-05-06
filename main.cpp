4ba9acf35d16a15697a970882806efc8fbeeedfc
Luis <Luisf311220@gmail.com>
2024-05-06 20:35:14 -0300

    feat: Adição do menu paciente

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void Tela_Principal();
+int Tela_Paciente();
 
 int main()
 {
@@ -11,7 +12,8 @@ int main()
 
 void Tela_Principal()
 {
-    int opcao_principal = 0;
+    int opcao_principal = 0; // variável para seleção no menu principal
+    int opcao_paciente = 0;  // variável para seleção no menu paciente
     // Tela principal
     do
     {
@@ -28,6 +30,7 @@ void Tela_Principal()
         cout << "|=============================================|" << endl;
         cout << "-----------------------------------------------" << endl;
         cin >> opcao_principal;
+        //------------------SELEÇÃO NO MENU PRINCIPAL-------------------------//
         switch (opcao_principal)
         {
         case 0:
@@ -35,6 +38,36 @@ void Tela_Principal()
             break;
         case 1:
             cout << "Abrindo módulo Paciente" << endl;
+            //------------------SELEÇÃO NO MENU PACIENTE-----------------------//
+            opcao_paciente = Tela_Paciente();
+            switch (opcao_paciente)
+            {
+            case 1:
+                cout << "Cadastro de Paciente" << endl;
+                break;
+            case 2:
+                cout << "Edição de Paciente" << endl;
+                break;
+            case 3:
+                cout << "Eliminação de Paciente" << endl;
+                break;
+            case 4:
+                cout << "Busca por Paciente" << endl;
+                break;
+            case 5:
+                cout << "Checagem de Consulta" << endl;
+                break;
+            case 6:
+                cout << "Pagamento de consulta" << endl;
+                break;
+            case 7:
+                cout << "Agendamento de consulta" << endl;
+                break;
+            default:
+                cout << "Escolha uma opção dentre as listadas" << endl;
+                break;
+            }
+            //------------------FIM DO MENU PACIENTE-----------------------//
             break;
         case 2:
             cout << "Abrindo módulo Profissional" << endl;
@@ -46,5 +79,29 @@ void Tela_Principal()
             cout << "Escolha uma opção dentre as listadas" << endl;
             break;
         }
+        //------------------FIM DO MENU PRINCIPAL-------------------------//
     } while (opcao_principal != 0);
+}
+
+int Tela_Paciente()
+{
+    int opcao_paciente = 0;
+    cout << "-----------------------------------------------" << endl;
+    cout << "|=============================================|" << endl;
+    cout << "|=================SIG-CLINIC==================|" << endl;
+    cout << "|=============================================|" << endl;
+    cout << "|++++++++++++++++++PACIENTES++++++++++++++++++|" << endl;
+    cout << "|=============================================|" << endl;
+    cout << "|== 0-Voltar                                ==|" << endl; // volta ao menu principal
+    cout << "|== 1-Cadastrar                             ==|" << endl; // cadastra um paciente
+    cout << "|== 2-Editar                                ==|" << endl; // edita as informações de um paciente
+    cout << "|== 3-Deletar                               ==|" << endl; // deleta um paciente
+    cout << "|== 4-Buscar                                ==|" << endl; // busca um paciente
+    cout << "|== 5-Checar Consulta                       ==|" << endl; // checa as consultas de um paciente
+    cout << "|== 6-Pagamento                             ==|" << endl; // paga uma consulta de um paciente
+    cout << "|== 7-Agendamento                           ==|" << endl; // agenda uma consulta para um paciente
+    cout << "|=============================================|" << endl;
+    cout << "-----------------------------------------------" << endl;
+    cin >> opcao_paciente;
+    return opcao_paciente;
 }
